94e548144156e7d3d03d9ffa28138b2d9f83bda7
Trisha Garrett <trisha.garrett@gmail.com>
2018-06-21 22:46:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+  attr_accessor
+
+  def initialize
+  end  
+
+  def kind(side_one, side_two, side_three)
+    if side_one == side_two && side_one == side_three & side_two == side_three
+      equilateral
+    elsif
+
+  end
+
 end

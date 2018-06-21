973febeba5c4d4ae6a46fbbc6211cf5352d430f3
Trisha Garrett <trisha.garrett@gmail.com>
2018-06-21 23:04:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,9 @@ class Triangle
     if side_one == side_two && side_one == side_three && side_two == side_three
       :equilateral
     elsif side_one == side_two || side_two == side_three || side_one == side_three
-      :isosceles  
-    elsif side_one != side_two && side_two != side_three && side_one != side_three  
+      :isosceles
+    elsif side_one != side_two && side_two != side_three && side_one != side_three
+      :scelene  
 
     end
   end
